c0c25b9e99beeb267131727da88a96778fb021f5
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-02-20 10:03:37 +0700

    Sua

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -12,24 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-<<<<<<< HEAD
-    public void Tru2So(View view) {
-        Intent intent = new Intent(MainActivity.this, Truactivity.class);
-        startActivity(intent);
+        public void Tru2So (View view){
+            Intent intent = new Intent(MainActivity.this, Truactivity.class);
+            startActivity(intent);
+        }
+
+
+        public void Tong (View view){
+            Intent intent = new Intent(MainActivity.this, tong2so.class);
+            startActivity(intent);
+        }
+
+
+        public void Nhan2so (View view){
+            Intent intent = new Intent(MainActivity.this, NhanActivity.class);
+            startActivity(intent);
+        }
+
+
     }
-=======
 
 
-    public void Tong(View view) {
-        Intent intent = new Intent(MainActivity.this, tong2so.class);
-        startActivity(intent);
-    }
 
-
-    public void Nhan2so(View view) {
-        Intent intent = new Intent(MainActivity.this, NhanActivity.class);
-        startActivity(intent);
-    }
-
->>>>>>> c809ef9774eb82c427c09ee7a924e1ccfd9fb594
-}
